c20619d5324add2a6d28b2efd7b940e32650d05b
Erik Aguayo <erik.aguayo2887@alumnos.udg.mx>
2020-09-30 19:58:08 -0500

    prueba 01

--- main.cpp
+++ main.cpp
@@ -1,9 +1,34 @@
 #include <iostream>
-
 using namespace std;
-
 int main() {
-    cout << "Hola Mundo!" << endl; // eliminar esta línea
+	
+    string val;
+    
+    getline(cin, val);
 
+    if (val == "int")
+    {
+    	cout << sizeof(int) << endl;
+	}
+	else if (val == "float")
+    {
+    	cout << sizeof(float) << endl;
+	}
+	else if (val == "char")
+    {
+    	cout << sizeof(char) << endl;
+	}
+	else if (val == "short")
+    {
+    	cout << sizeof(short) << endl;
+	}
+	else if (val == "double")
+    {
+    	cout << sizeof(double) << endl;
+	}
+	else if (val == "long")
+    {
+    	cout << sizeof(long) << endl;
+	}
     return 0;
 }
